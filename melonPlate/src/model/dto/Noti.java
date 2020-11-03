aa91176e8db87784c92c7def0783b38143b606e7
KOSTA <KOSTA@192.168.0.106>
2020-11-03 19:02:13 +0900

    hi

--- melonPlate/src/model/dto/Noti.java
+++ melonPlate/src/model/dto/Noti.java
@@ -1,5 +1,70 @@
 package model.dto;
 
 public class Noti {
+	private int notiNo;// 공지사항 No
+	private int userNo;// 유저 No
+	private String notiTitle;// 공지사항 제목
+	private String notiDate;// 공지사항 작성날짜
+	private String notiContent;// 공지사항 내용
+	private String notiHits;// 공지사항 조회수
+
+	
+	public Noti() {}
+	public Noti(int notiNo, int userNo, String notiTitle, String notiDate, String notiContent, String notiHits) {
+		this.notiNo = notiNo;
+		this.userNo = userNo;
+		this.notiTitle = notiTitle;
+		this.notiDate = notiDate;
+		this.notiContent = notiContent;
+		this.notiHits = notiHits;
+	}
+
+	public int getNotiNo() {
+		return notiNo;
+	}
+
+	public void setNotiNo(int notiNo) {
+		this.notiNo = notiNo;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getNotiTitle() {
+		return notiTitle;
+	}
+
+	public void setNotiTitle(String notiTitle) {
+		this.notiTitle = notiTitle;
+	}
+
+	public String getNotiDate() {
+		return notiDate;
+	}
+
+	public void setNotiDate(String notiDate) {
+		this.notiDate = notiDate;
+	}
+
+	public String getNotiContent() {
+		return notiContent;
+	}
+
+	public void setNotiContent(String notiContent) {
+		this.notiContent = notiContent;
+	}
+
+	public String getNotiHits() {
+		return notiHits;
+	}
+
+	public void setNotiHits(String notiHits) {
+		this.notiHits = notiHits;
+	}
 
 }
--- melonPlate/src/model/dto/User.java
+++ melonPlate/src/model/dto/User.java
@@ -1,5 +1,111 @@
 package model.dto;
 
 public class User {
-	
+	private int userNo;
+	private String nickname;
+	private int age;
+	private String email;
+	private String gender;
+	private String grade;
+	private int point;
+	private String prefer;
+	private int attend;
+	private int usage;
+
+	public User() {
+	}
+
+	public User(int userNo, String nickname, int age, String email, String gender, String grade, int point,
+			String prefer, int attend, int usage) {
+		this.userNo = userNo;
+		this.nickname = nickname;
+		this.age = age;
+		this.email = email;
+		this.gender = gender;
+		this.grade = grade;
+		this.point = point;
+		this.prefer = prefer;
+		this.attend = attend;
+		this.usage = usage;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public String getPrefer() {
+		return prefer;
+	}
+
+	public void setPrefer(String prefer) {
+		this.prefer = prefer;
+	}
+
+	public int getAttend() {
+		return attend;
+	}
+
+	public void setAttend(int attend) {
+		this.attend = attend;
+	}
+
+	public int getUsage() {
+		return usage;
+	}
+
+	public void setUsage(int usage) {
+		this.usage = usage;
+	}
 }
